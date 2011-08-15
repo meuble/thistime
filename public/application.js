262d69d129e1da9982f144da1d11148995435b6d
Stéphane Akkaoui <stephane.akkaoui@imeuble.info>
2011-08-15 23:06:03 +0200

    Fix firefox bugs...

--- public/application.js
+++ public/application.js
@@ -2,7 +2,7 @@ function findSize() {
 	var height = $(window).height();
 	var width = $(window).width();
 	var widePhotoWidth = $(window).height() * 1.5
-	
+
 	if (width < widePhotoWidth + 250) {
 		$('#wrapper').removeClass('wide');
 		$('#wrapper').addClass('small');
@@ -10,12 +10,13 @@ function findSize() {
 		$('#wrapper').removeClass('small');		
 		$('#wrapper').addClass('wide');
 	}
-	
+
 	$('.photos').css('width', widePhotoWidth > width ? width : widePhotoWidth);
 	$('.photo').each(function(index, element) {
 		var article = $(element);
 		var image = $('img', element);
-		var photoWidth = parseInt(image.css('width'))
+		var photoWidth = image.attr('src') ? parseInt(image.css('width')) : widePhotoWidth
+
 		if (photoWidth > width) {
 			image.css('marginLeft', (width - photoWidth) / 2)
 		} else {
@@ -25,9 +26,13 @@ function findSize() {
 	});	
 }
 
-$(function() {
-	$(window).resize(findSize);
-	$(window).load(findSize);
+$(window).resize(findSize);
+$(window).load(function() {
+	findSize();
+	$('footer').fadeOut(500);
+});
 
+
+$(function() {
 	$("article.photo").hover(function() { $('footer', this).fadeIn(500); }, function() { $('footer', this).fadeOut(500); });
 });
